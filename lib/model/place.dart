dc853bb19c6729c77e870279b16f6b0f27ee200b
phuchub <phuchmth2209055@fpt.edu.vn>
2024-09-09 11:41:50 +0700

    new

--- lib/model/place.dart
+++ lib/model/place.dart
@@ -1,23 +1,14 @@
 class Place {
-  final int id;
   final String name;
-  final double rating;
   final String imageUrl;
 
-  Place({
-    required this.id,
-    required this.name,
-    required this.rating,
-    required this.imageUrl,
-  });
-
+  Place({required this.name, required this.imageUrl});
 
   factory Place.fromJson(Map<String, dynamic> json) {
     return Place(
-      id: json['id'],
       name: json['name'],
-      rating: double.parse(json['rating']),
       imageUrl: json['imageUrl'],
     );
   }
 }
+
